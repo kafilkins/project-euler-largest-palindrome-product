e353765d4250754f0e817a1f570a669271a3d571
Keith Filkins <kafilkins@me.com>
2021-09-16 11:14:41 -0400

    commit tree 74f6ab080857d719157f03a6a691ad9e59530398

--- lib/oo_largest_palindrome_product.rb
+++ lib/oo_largest_palindrome_product.rb
@@ -1,1 +1,29 @@
-# Implement your object-oriented solution here!
+class LargestPalindromeProduct 
+    attr_accessor :answer, :input_1, :input_2
+
+    def initialize
+        @answer = palindrome_product 
+        @input_1 = 0
+        @input_2 = 0
+    end
+    
+    def is_palindrome?(input)
+        input.to_s == input.to_s.reverse 
+    end
+
+    def palindrome_product 
+        max = 0 
+
+        (100...999).each do |first|
+            (100...999).each do |second|
+                product = first * second 
+                if is_palindrome?(product) && product > max 
+                    self.input_1, self.input_2 = first, second 
+                    max = product 
+                end
+            end
+        end
+        max 
+    end
+
+end
